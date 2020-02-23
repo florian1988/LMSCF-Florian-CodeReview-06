657bb0cc981172394de8fe4c8e0941891fde992e
Florian Mörzinger <flo.moerzinger@gmx.at>
2020-02-23 10:22:00 +0100

    update

--- JS/main.ts
+++ JS/main.ts
@@ -175,13 +175,6 @@ document.getElementsByClassName("showevents")[0].innerHTML += (events[i].mainEve
 
 
 
-
-
-
-
-
-
-
  $('.movementRestaurant').mousemove(function(e) {
     var x = (e.pageX * -1 / 5), y = (e.pageY * -1 / 5);
     $(this).css('background-position', x + 'px ' + y + 'px');
